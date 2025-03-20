6edff1d146b25f6f2b7d1a53c8d26de97c64c417
Moret Camille <moretcamille@orange.fr>
2025-03-20 18:57:38 +0100

    error fait

--- projet8/_utils/Error.jsx
+++ projet8/_utils/Error.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link, Links } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import "./Error.css"
 
 const Error = () => {
     return (
         <div>
-            L'erreur
+            <Header />
+            <div className='styleError'>
+                <p className='error404'>404</p>
+                <p className='errorOups'>Oups ! La page que vous demandez n'existe pas.</p>
+                <p className='goBackHome'><Link to="/Home">Retourner sur la page d'acceuil</Link></p>
+            </div>
+            <Footer />
         </div>
     );
 };
